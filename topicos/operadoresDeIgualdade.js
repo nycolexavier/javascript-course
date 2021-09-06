856e72e17a0f1390c57bf752a72c74ddfe5a1d58
Nycole <nycole.xavierr@gmail.com>
2021-09-06 14:43:01 -0300

    exercise operadores de igualdades

---
+++ topicos/operadoresDeIgualdade.js
@@ -0,0 +1,9 @@
+// operadores de igualdade
+// igualdade estrita
+console.log(1 === 1)
+console.log('1' === 1)
+
+// Igualdade solta
+// ignora o tipo e vê os valores
+console.log (1 == 1)
+console.log( '1' == 1)
