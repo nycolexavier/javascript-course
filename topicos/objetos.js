f914b599e1aa137c562c69a66c3fd01641f15146
Nycole <nycole.xavierr@gmail.com>
2021-09-06 11:19:48 -0300

    finish exercise objetos

---
+++ topicos/objetos.js
@@ -0,0 +1,18 @@
+let nome = 'Rafael';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome;
+let corSelecionado = null;
+
+// Exercício - Objeto
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de souza',
+    rua: 'Josefina',
+    cidade: 'beaga',
+    livro: 'Bíblia'
+};
+
+console.log(pessoa);
