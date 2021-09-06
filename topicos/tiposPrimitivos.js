48b2f249ff39630708320dacc00c00643b07110d
Nycole <nycole.xavierr@gmail.com>
2021-09-06 11:06:42 -0300

    finish exercise tipos primitivos

---
+++ topicos/tiposPrimitivos.js
@@ -0,0 +1,14 @@
+// dois tipos primitivos:
+// referencia e de valores
+
+// string, number, underfine e null
+
+let nome = 'Rafael' // string literal - armazena informação em forma de string
+
+let idade = 25; // number literal
+
+let estaAprovado = true; // boolean
+
+let sobrenome ; // Undefined
+
+let corSelectionado = null // redefinirum valor
