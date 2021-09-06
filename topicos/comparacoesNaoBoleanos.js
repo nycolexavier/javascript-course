3724287f58b5b687673064e581ef570014d29299
Nycole <nycole.xavierr@gmail.com>
2021-09-06 15:00:04 -0300

    finish exercise comparacoes nao boleanos

---
+++ topicos/comparacoesNaoBoleanos.js
@@ -0,0 +1,15 @@
+// falsy
+// undefined
+// null
+// 0
+// false
+// ''
+// NaN = not a number
+
+// Truthy
+
+let corPersonalizada = 'vermelho';
+let corPadrao = 'Azul'
+let corPerfil = corPersonalizada || corPadrao;
+
+console.log(corPerfil)
