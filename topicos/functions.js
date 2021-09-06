2605f5b5dd657cbe67284961713b8d5591e6f199
Nycole <nycole.xavierr@gmail.com>
2021-09-06 11:37:42 -0300

    finish exercise functions

---
+++ topicos/functions.js
@@ -0,0 +1,21 @@
+// Verbo ++ substantivo
+
+let corSite = "Azul";
+function resetaCor (cor, tonalidade) {
+    corSite = cor + tonalidade;
+};
+
+console.log(corSite);
+resetaCor('verde', ' claro')
+console.log(corSite);
+
+
+// Exercício - Functions
+corBlog = 'verde'
+function altereAlgo (cor, tonalidade) {
+    corBlog = cor + tonalidade;
+}
+
+console.log(corBlog);
+altereAlgo('cinza', ' bege');
+console.log(corBlog);
