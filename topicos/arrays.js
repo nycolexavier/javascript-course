de47c5c18943c1cc524807023db5cf2742a78ebf
Nycole <nycole.xavierr@gmail.com>
2021-09-06 11:26:23 -0300

    finish exercise arrays

---
+++ topicos/arrays.js
@@ -0,0 +1,10 @@
+// Arrays
+//  Conjunto de dados que pode ser acessado por um índice
+
+let familia = [25, 45, 46, 'nome' ,36]
+console.log(familia.length)
+console.log(familia[3]);
+
+// Exercício - Arrays
+let nomeDaColega = ['Rute', 30, 'moabita'];
+console.log(nomeDaColega);
