915475506ee7a972c233ec2e65ec9593de1f48fb
Nycole <nycole.xavierr@gmail.com>
2021-09-06 14:23:24 -0300

    finish exercise tipos de funções

---
+++ topicos/tipoDeFuncoes.js
@@ -0,0 +1,16 @@
+// Realiza uma tarefa, não devolve nada
+function dizerNome() {
+    console.log('nome')
+};
+
+dizerNome();
+
+// faz um calculo ou operaçao e retorna algo
+function multiplicarPorDois (valor) {
+    return valor * 2;
+}
+
+// console.log(multiplicarPorDois(5))
+
+let resultado = multiplicarPorDois(5)
+console.log(resultado)
