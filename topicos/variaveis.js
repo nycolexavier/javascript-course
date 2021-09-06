bc04d6966cb514c6db56d3f1e173f18b847c6341
Nycole <nycole.xavierr@gmail.com>
2021-09-06 10:58:11 -0300

    finish exercise variaveis

---
+++ topicos/variaveis.js
@@ -0,0 +1,20 @@
+// let : tipo da variável
+// idade: nome da variável
+// = atribuição:
+
+
+let idadee = 5;
+let outrovalor = 10;
+
+// mostrar no console
+console.log('jonathan' + outrovalor)
+
+
+// Exercício - Variáveis:
+// mostrar três variáveis diferentes
+
+let nome = 'maria';
+let idade = 90;
+let cidade = 'beagá';
+
+console.log(nome, idade, cidade)
