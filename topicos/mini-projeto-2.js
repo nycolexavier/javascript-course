409530de2c01a821f6e73c52bc3ca4cfcbd9d3b9
Nycole <nycole.xavierr@gmail.com>
2021-09-08 10:00:27 -0300

    finish mini projeto - 2

---
+++ topicos/mini-projeto-2.js
@@ -0,0 +1,12 @@
+// Máximo entre dois valores
+
+// escreva uma função que usa dois números e retorna o maior entre eles
+
+
+let valorMaior = mostrarMaior(5,9)
+function mostrarMaior (numero1, numero2) {
+    if (numero1 > numero2) {
+        return numero1
+    }
+    else return numero2
+}
