775fed4767282acce8c7fe4e25dc817f4aff53a9
Nycole <nycole.xavierr@gmail.com>
2021-09-08 09:15:47 -0300

    finish exercise laco-loop-for

---
+++ topicos/laco-loop-for.js
@@ -0,0 +1,12 @@
+// 1. For
+// for (let i = 0; i >= 1; i--) {
+//     if(i % 2 !== 0){ // modules
+//         console.log(i)
+//     }
+    
+    
+// }
+// 2. While
+// 3. Do.. while
+// 4. For..In
+// 5. For..of
